c8534f286b534bfe39cf9fcb20c65a3bf56b31c8
U-ASAV\OCapovilla <OCapovilla@895331-U.asav.brm>
2025-05-31 14:36:01 -0300

    Atividade vivencial m3 pt 1

--- src/m3.cpp
+++ src/m3.cpp
@@ -4,6 +4,7 @@
 
 #include <algorithm>
 #include <iostream>
+#include <string>
 #include <vector>
 
 #include "GLFW/glfw3.h"
@@ -14,6 +15,9 @@
 #include "glm/vec2.hpp"
 #include "glm/vec3.hpp"
 
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
 constexpr int WIDTH = 800;
 constexpr int HEIGHT = 600;
 
@@ -23,52 +27,33 @@ struct Triangle {
     glm::vec3 color;
 };
 
-constexpr glm::vec3 colors[] = {
-    glm::vec3(1.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f),
-    glm::vec3(0.0f, 0.0f, 1.0f), glm::vec3(1.0f, 1.0f, 0.0f),
-    glm::vec3(1.0f, 0.0f, 1.0f), glm::vec3(0.0f, 1.0f, 1.0f),
-    glm::vec3(1.0f, 1.0f, 1.0f), glm::vec3(0.5f, 0.5f, 0.5f),
-    glm::vec3(0.2f, 0.2f, 0.2f), glm::vec3(0.8f, 0.8f, 0.8f),
-    glm::vec3(1.0f, 0.5f, 0.0f), glm::vec3(0.8f, 0.4f, 0.1f),
-    glm::vec3(0.9f, 0.6f, 0.2f), glm::vec3(0.7f, 0.3f, 0.0f),
-    glm::vec3(1.0f, 0.7f, 0.5f), glm::vec3(0.5f, 0.0f, 0.5f),
-    glm::vec3(0.6f, 0.2f, 0.8f), glm::vec3(0.4f, 0.0f, 0.6f),
-    glm::vec3(0.1f, 0.8f, 0.9f), glm::vec3(0.2f, 0.6f, 0.8f),
-    glm::vec3(0.0f, 0.5f, 0.5f), glm::vec3(0.4f, 0.6f, 0.2f),
-    glm::vec3(0.6f, 0.4f, 0.2f), glm::vec3(0.7f, 0.5f, 0.3f),
-    glm::vec3(0.5f, 0.5f, 0.0f), glm::vec3(0.3f, 0.3f, 0.3f),
-    glm::vec3(1.0f, 0.8f, 0.8f), glm::vec3(0.8f, 1.0f, 0.8f),
-    glm::vec3(0.8f, 0.8f, 1.0f), glm::vec3(1.0f, 1.0f, 0.8f),
-    glm::vec3(1.0f, 0.8f, 1.0f), glm::vec3(0.8f, 1.0f, 1.0f)};
-
-glm::vec3 randomColor() {
-    return colors[rand() % sizeof(colors) / sizeof(colors[0])];
-}
 
 std::vector<Triangle> triangles;
 glm::vec2 nextTrianglePosition[3] = {};
 int currentVertice = 0;
 
 constexpr auto vertexShaderSource = R"GLSL(
-#version 330 core
-layout (location = 0) in vec3 aPos;
-
-void main()
-{
-    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
-}
-)GLSL";
+ #version 400
+ layout (location = 0) in vec3 position;
+ layout (location = 1) in vec2 texc;
+ out vec2 tex_coord;
+ void main()
+ {
+	tex_coord = vec2(texc.s, texc.t);
+	gl_Position = vec4(position, 1.0);
+ }
+ )GLSL";
 
 constexpr auto fragmentShaderSource = R"GLSL(
-#version 330 core
-uniform vec4 inputColor;
-out vec4 FragColor;
-
-void main()
-{
-    FragColor = inputColor;
-}
-)GLSL";
+ #version 400
+ in vec2 tex_coord;
+ out vec4 color;
+ uniform sampler2D tex_buff;
+ void main()
+ {
+	 color = texture(tex_buff,tex_coord);
+ }
+ )GLSL";
 
 void framebuffer_size_callback(GLFWwindow* window, const int width,
                                const int height) {
@@ -132,38 +117,83 @@ GLuint createVBOAndBind(const GLuint VAO, const float* vertices,
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, verticesLength * sizeof(float), vertices,
-                 GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, verticesLength * sizeof(float), vertices, GL_STATIC_DRAW);
 
     return VBO;
 }
 
-Triangle setupGeometry(const float x1, const float y1, const float x2,
-                        const float y2, const float x3, const float y3) {
+GLuint setupSprite(int size) {
     GLuint VAO;
     glGenVertexArrays(1, &VAO);
 
-    float vertices[] = {
-        x1, y1, 0.0f, x2, y2, 0.0f, x3, y3, 0.0f,
+    GLfloat vertices[] = {
+        // x   y    z    s     t
+        -0.5,  0.5, 0.0, 0.0, 1.0, //V0
+        -0.5, -0.5, 0.0, 0.0, 0.0, //V1
+        0.5,  0.5, 0.0, 1.0, 1.0, //V2
+        0.5, -0.5, 0.0, 1.0, 0.0  //V3
     };
 
-    createVBOAndBind(VAO, vertices, sizeof(vertices) / sizeof(float));
+    for (int i = 0; i < sizeof(vertices) / sizeof(vertices[0]); i++) {
+        vertices[i] *= size;
+    }
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float),
-                          (void*)0);
+    createVBOAndBind(VAO, vertices, sizeof(vertices) / sizeof(vertices[0]));
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
-    Triangle t;
-    t.VAO = VAO;
-    t.color = randomColor();
-
-    triangles.push_back(t);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid *)(3 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(1);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
 
-    return t;
+    return VAO;
 }
+
+GLuint loadTexture(std::string filePath)
+{
+	GLuint texID;
+
+	// Gera o identificador da textura na memória
+	glGenTextures(1, &texID);
+	glBindTexture(GL_TEXTURE_2D, texID);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+	int width, height, nrChannels;
+
+	unsigned char *data = stbi_load(filePath.c_str(), &width, &height, &nrChannels, 0);
+
+	if (data)
+	{
+		if (nrChannels == 3) // jpg, bmp
+		{
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		}
+		else // png
+		{
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+		}
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "Failed to load texture" << std::endl;
+	}
+
+	stbi_image_free(data);
+
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+	return texID;
+}
+
 
 int main() {
     glfwInit();
@@ -171,8 +201,9 @@ int main() {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Vertices por clique",
-                                          nullptr, nullptr);
+    glfwWindowHint(GLFW_SAMPLES, 8);
+
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Parallax", nullptr, nullptr);
 
     if (window == nullptr) {
         std::cout << "Failed to create GLFW window" << std::endl;
@@ -190,36 +221,17 @@ int main() {
     glViewport(0, 0, WIDTH, HEIGHT);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    glfwSetMouseButtonCallback(
-        window, [](GLFWwindow* window, int button, int action, int mods) {
-            if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS) {
-                double xpos, ypos;
-                glfwGetCursorPos(window, &xpos, &ypos);
-
-                nextTrianglePosition[currentVertice] = glm::vec2(
-                    -1 * (1 - (2 * xpos / WIDTH)), (1 - (2 * ypos / HEIGHT)));
-
-                currentVertice += 1;
-
-                if (currentVertice == 3) {
-                    Triangle triangle =
-                        setupGeometry((float)nextTrianglePosition[0].x,
-                                       (float)nextTrianglePosition[0].y,
-                                       (float)nextTrianglePosition[1].x,
-                                       (float)nextTrianglePosition[1].y,
-                                       (float)nextTrianglePosition[2].x,
-                                       (float)nextTrianglePosition[2].y);
-
-                    currentVertice = 0;
-                }
-            }
-        });
-
     const GLuint shaderProgram = createShaderProgram();
-
-    GLint colorLoc = glGetUniformLocation(shaderProgram, "inputColor");
+	GLuint VAO = setupSprite(10);
+	GLuint texID = loadTexture("../assets/1.png");
 
     glUseProgram(shaderProgram);
+    glActiveTexture(GL_TEXTURE0);
+    glUniform1i(glGetUniformLocation(shaderProgram, "tex_buff"), 0);
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_ALWAYS); 
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
@@ -228,22 +240,14 @@ int main() {
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        for (const Triangle t : triangles) {
-            glBindVertexArray(t.VAO);
-
-            glUniform4f(colorLoc, t.color.r, t.color.g, t.color.b, 1.0f);
-
-            glDrawArrays(GL_TRIANGLES, 0, 3);
-            glBindVertexArray(0);
-        }
+        glBindVertexArray(VAO); // Conectando ao buffer de geometria
+        glBindTexture(GL_TEXTURE_2D, texID); // Conectando ao buffer de textura
+		glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 
         glfwSwapBuffers(window);
     }
 
-    for (const Triangle t : triangles) {
-        glDeleteVertexArrays(1, &t.VAO);
-    }
-
+	glDeleteVertexArrays(1, &VAO);
     glDeleteProgram(shaderProgram);
 
     glfwTerminate();
